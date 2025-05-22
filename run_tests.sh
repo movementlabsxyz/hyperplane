58953ef99acb08b07679e1bc4d86bccc66e9c8c0
apenzk <dr.andreas.penzkofer@gmail.com>
2025-05-22 08:55:12 +0200

    move test files

--- run_tests.sh
+++ run_tests.sh
@@ -26,39 +26,39 @@ TESTS=(
     setup_with_mpsc::v13::test_v13
 
     # - - - CL tests - - -
-    confirmation_layer::basic::test_regular_transactions
-    confirmation_layer::basic::test_basic_confirmation_layer
-    confirmation_layer::basic::test_block_interval
-    confirmation_layer::basic::test_register_chain
-    confirmation_layer::basic::test_get_current_block
-    confirmation_layer::basic::test_get_subblock
-    confirmation_layer::basic::test_submit_transaction
-    confirmation_layer::basic::test_set_block_interval
-    confirmation_layer::basic::test_invalid_block_interval
-    confirmation_layer::basic::test_chain_not_found
-    confirmation_layer::basic::test_chain_already_registered
-    confirmation_layer::basic::test_chain_registration
-    confirmation_layer::basic::test_block_interval_validation
-    confirmation_layer::basic::test_subblock_not_found
-    confirmation_layer::basic::test_get_registered_chains
-    confirmation_layer::basic::test_get_block_interval
-    confirmation_layer::basic::test_submit_transaction_chain_not_registered
-    confirmation_layer::basic::test_submit_cl_transaction_for_multiple_chains
+    confirmation_layer::node::test_regular_transactions
+    confirmation_layer::node::test_basic_confirmation_layer
+    confirmation_layer::node::test_block_interval
+    confirmation_layer::node::test_register_chain
+    confirmation_layer::node::test_get_current_block
+    confirmation_layer::node::test_get_subblock
+    confirmation_layer::node::test_submit_transaction
+    confirmation_layer::node::test_set_block_interval
+    confirmation_layer::node::test_invalid_block_interval
+    confirmation_layer::node::test_chain_not_found
+    confirmation_layer::node::test_chain_already_registered
+    confirmation_layer::node::test_chain_registration
+    confirmation_layer::node::test_block_interval_validation
+    confirmation_layer::node::test_subblock_not_found
+    confirmation_layer::node::test_get_registered_chains
+    confirmation_layer::node::test_get_block_interval
+    confirmation_layer::node::test_submit_transaction_chain_not_registered
+    confirmation_layer::node::test_submit_cl_transaction_for_multiple_chains
     
     # - - - HIG tests - - -
-    hyper_ig::basic::test_regular_transaction_success
-    hyper_ig::basic::test_regular_transaction_failure
-    hyper_ig::basic::test_regular_transaction_pending
-    hyper_ig::basic::test_cat_success_proposal
-    hyper_ig::basic::test_cat_failure_proposal
-    hyper_ig::basic::test_get_pending_transactions
-    hyper_ig::basic::test_wrong_chain_subblock
+    hyper_ig::node::test_regular_transaction_success
+    hyper_ig::node::test_regular_transaction_failure
+    hyper_ig::node::test_regular_transaction_pending
+    hyper_ig::node::test_cat_success_proposal
+    hyper_ig::node::test_cat_failure_proposal
+    hyper_ig::node::test_get_pending_transactions
+    hyper_ig::node::test_wrong_chain_subblock
 
     # - - - HS tests - - -
-    hyper_scheduler::basic::test_receive_success_proposal_first_message
-    hyper_scheduler::basic::test_receive_failure_proposal_first_message
-    hyper_scheduler::basic::test_duplicate_rejection
-    hyper_scheduler::basic::test_process_proposals_for_two_chain_cat
+    hyper_scheduler::node::test_receive_success_proposal_first_message
+    hyper_scheduler::node::test_receive_failure_proposal_first_message
+    hyper_scheduler::node::test_duplicate_rejection
+    hyper_scheduler::node::test_process_proposals_for_two_chain_cat
     
     # - - - CL to HIG tests - - -
     integration::cl_to_hig::channels::test_process_subblock_with_regular_transaction_success
@@ -69,7 +69,7 @@ TESTS=(
     integration::hs_to_cl::channels::test_single_chain_cat_status_update
     integration::hs_to_cl::channels::test_several_single_chain_cat_status_updates
 
-    ## - - - CL to HS tests - - -
+    # - - - CL to HS tests - - -
     integration::cl_to_hs::channels::test_cat_one_cat_success
     integration::cl_to_hs::channels::test_cat_one_cat_failure
 
@@ -82,11 +82,8 @@ TESTS=(
     integration::e2e::channels::test_single_chain_cat_failure
 )
 
-
-
 TESTS2=(
-
-integration::cl_to_hs::channels::test_cat_one_cat_success
+    confirmation_layer::node::test_regular_transactions
 )
 
 # Run the appropriate test set based on the input
